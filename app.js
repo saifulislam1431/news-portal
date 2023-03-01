c53d567cc15aacb65aa70a760ed965d64d2fc5cd
Saiful Islam <sakibbhaiya143@gmail.com>
2023-03-02 00:59:50 +0600

    API data filtering added

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+let fetchData = []
+    // Get Categories
 const getAllCategories = () => {
     fetch('https://openapi.programming-hero.com/api/news/categories')
-        .then(res => res.json()).then(data => displayCategories(data.data))
-}
+        .then(res => res.json())
+        .then(data =>
+            displayCategories(data.data)
+        );
+
+
+};
 
 const displayCategories = (categories) => {
     // console.log(categories);
@@ -12,37 +19,47 @@ const displayCategories = (categories) => {
         `
     })
 }
+
+// Get Categories Id
 const getCategoryId = (id, name) => {
     const url = ` https://openapi.programming-hero.com/api/news/category/${id}`
-    fetch(url).then(res => res.json()).then(data => displayAlert(data.data, name))
+    fetch(url).then(res => res.json()).then(data => {
+        fetchData = data.data
+        displayAlert(data.data, name)
+    })
+
 
 }
 const displayAlert = (items, name) => {
     document.getElementById('item-found').innerText = items.length
     document.getElementById('category-name').innerText = name
     const allNews = document.getElementById('news-container')
+
     document.getElementById('news-container').innerHTML = ''
     items.forEach(item => {
+        const date = new Date(item.author.published_date)
         allNews.innerHTML += `
         <div class="card card-side bg-base-100 shadow-xl rounded-lg mb-4 mt-4">
                     <figure><img src="${item.thumbnail_url}" alt="Movie" /></figure>
                     <div class="card-body">
-                        <h2 class="card-title">${item.title}</h2>
+                        <h2 class="card-title">${item.title}<div class="badge badge-warning gap-2">
+                        ${item.others_info.is_trending ?  "Trending": ''}
+                      </div></h2>
                         <p class="pt-2 pb-2">${item.details.slice(0,150)}......</p>
                         <div class="card-actions flex justify-between items-center">
                         <div class="flex justify-center items-center gap-3">
                         <img src="${item.author.img}" class="w-12 rounded-full" alt="author img">
                         <div>
-                        <p class="text-lg font-semibold">${item.author.name}</p>
-                        <p>${item.author.published_date}</p>
+                        <p class="text-lg font-semibold">${item.author.name ?  item.author.name:"Not available"}</p>
+                        <p>${date.toDateString()}</p>
                         </div>
                         </div>
                         <div class="font-semibold text-lg text-blue-700 flex justify-center items-center gap-3">
                         <i class="fa-solid fa-eye"></i>
-                        <p>${item.total_view}</p>
+                        <p>${item.total_view ? item.total_view :"Not available" }</p>
                         </div>
                             <div>
-                             <p class="text-lg font-semibold">Rating: <span class="text-green-400">${item.rating.badge}</span></p>
+                             <p class="text-lg font-semibold">Rating: <span class="text-green-400">${item.rating.number}</span></p>
                             </div>
                            <div>
                            <label for="my-modal-3" onclick="loadModalDetails('${item._id}')" class="btn  border-blue-600 bg-transparent text-blue-700 font-semibold text-lg hover:bg-blue-600 hover:text-white outline-none"><i class="fa-solid fa-right-long"></i></label>
@@ -72,5 +89,19 @@ const displayModalDetails = (allDetails) => {
     titleOFNews.innerText = title;
     description.innerText = details;
 
+}
+
+// Show picks after clicking button
+const showTodayPick = () => {
+    let todayPicks = fetchData.filter(singleData => singleData.others_info.is_todays_pick === true);
+    const cateName = document.getElementById('category-name').innerText;
+    displayAlert(todayPicks, cateName)
+}
+
+// Show trending after clicking button
+const showTrending = () => {
+    let todayTrending = fetchData.filter(singleData => singleData.others_info.is_trending === true);
+    const cateName = document.getElementById('category-name').innerText;
+    displayAlert(todayTrending, cateName)
 }
 getAllCategories()
